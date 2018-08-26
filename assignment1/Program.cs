81a6ece2e731463191eeebdc37884a498487b760
Kyle G. Nally <kylegnally@hotmail.com>
2018-08-26 15:01:16 -0400

    Add program code.

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -8,9 +8,40 @@ namespace assignment1
 {
     class Program
     {
+        static UserInterface aMenu = UserInterface.GetInstance();
+
         static void Main(string[] args)
         {
+            ShowMenu();
+        }
 
+        static void ShowMenu()
+        {
+            aMenu.DrawMenu();
+            Console.Write(aMenu.MenuOutput);
+            GetInput();
+        }
+
+        static void GetInput()
+        {
+            Console.Write(aMenu.Spaces);
+            String userSelection = Console.ReadLine();
+            if (userSelection.ToUpper() == "Q")
+            {
+                Console.WriteLine(aMenu.ExitMessage);
+                System.Threading.Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            aMenu.GetChoice(userSelection);
+            ShowOutput();
+        }
+
+        static void ShowOutput()
+        {
+            Console.Write(aMenu.UserChoice);
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            ShowMenu();
         }
     }
 }
