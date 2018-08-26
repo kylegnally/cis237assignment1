135d58e9d91b5a3d7b87de0c4a116e92cad21778
Kyle G. Nally <kylegnally@hotmail.com>
2018-08-26 16:42:26 -0400

    Add code to WineItem class.

--- assignment1/WineItem.cs
+++ assignment1/WineItem.cs
@@ -8,15 +8,20 @@ namespace assignment1
 {
     class WineItem
     {
-        private String _id;
-        private String _description;
-        private String _pack;
+        private string _id;
+        private string _description;
+        private string _pack;
 
-        public WineItem(String Id, String Desc, String Pack)
+        public WineItem(string Id, string Desc, string Pack)
         {
             _id = Id;
             _description = Desc;
             _pack = Pack;
         }
+
+        public string Id { get; set; }
+        public string Desc { get; set; }
+        public string Pack { get; set; }
+
     }
 }
